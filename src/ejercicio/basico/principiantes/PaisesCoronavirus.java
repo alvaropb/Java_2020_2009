0fdc893fce1b12b8b64a725ecd0570021b0a5e80
Alvaro Parga <apargatrabajo@gmail.com>
2020-03-02 12:37:09 +0100

    Subida inicial

--- src/ejercicio/basico/principiantes/PaisesCoronavirus.java
+++ src/ejercicio/basico/principiantes/PaisesCoronavirus.java
@@ -1,19 +1,20 @@
 package ejercicio.basico.principiantes;
 
 public class PaisesCoronavirus {
-	/**
-	 * 1º recorrer paises, preguntar si existe algun caso de coronavirus en caso
-	 * afirmativo, preguntar numero de infectados Finalmente, mostrar paises libres
-	 * de coronavirus Mostrar paises infectados junto cpn su numero de casos. Total
-	 * de personas infectadas
-	 * 
-	 * @param args
-	 */
+/**
+ *1º recorrer paises, preguntar si existe algun caso de coronavirus
+ *en caso afirmativo, preguntar numero de infectados
+ *Finalmente, mostrar paises libres de coronavirus
+ *Mostrar paises infectados junto cpn su numero de casos.
+ *Total de personas infectadas 
+ * @param args
+ */
 	public static void main(String[] args) {
-		final String[] PAISES = { "Noruega", "Alemania", "Rep Checa", "Italia" };
-		boolean[] paisesInfectados = { false, false, false, false, false };
-		int[] numeroInfectados = { 0, 0, 0, 0, 0 };
+		final String[] PAISES= {"Noruega","Alemania","Rep Checa","Italia"};
+		boolean [] paisesInfectados= {false,false,false,false,false};
+		int [] numeroInfectados= {0,0,0,0,0};
+		
 
-	}// fin main
+	}//fin main
 
 }
