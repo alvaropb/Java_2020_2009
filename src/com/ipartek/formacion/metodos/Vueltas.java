1fae7a52834762cd496148e904cd4051c4ebe7b6
Alvaro Parga Barreiro <apargatrabajo@gmail.com>
2020-03-11 14:03:32 +0100

    Arreglado fallo con redondeo de decimales, falta corregir que el resto sea un numero decimal de 2 digitos

--- src/com/ipartek/formacion/metodos/Vueltas.java
+++ src/com/ipartek/formacion/metodos/Vueltas.java
@@ -1,5 +1,8 @@
 package com.ipartek.formacion.metodos;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class Vueltas {
 
 	public static final float[] BILLETES_MONEDAS = { 500f, 200f, 100f, 50f, 20f, 10f, 5f, 2f, 1f, 0.50f, 0.20f, 0.10f,
@@ -8,16 +11,24 @@ public class Vueltas {
 	public static int[] calcularVueltasOptimas(float importe, float entregado) throws Exception {
 
 		int[] vueltas = new int[BILLETES_MONEDAS.length];
-
+		DecimalFormat df = new DecimalFormat("#,###.###");
+		df.setRoundingMode(RoundingMode.HALF_EVEN);
 		// TODO vuestro marron
-		float resto = entregado;
+		float resto = 0f;
+
 		try {
-			calcularVueltas(importe, entregado);
+			resto = calcularVueltas(importe, entregado);
+			df.format(resto);
+			resto = Float.parseFloat(df.format(resto).replace(',', '.'));
+
 			for (int i = 0; i < BILLETES_MONEDAS.length; i++) {
 				if (BILLETES_MONEDAS[i] <= resto) {
 					int monedicas = (int) (resto / BILLETES_MONEDAS[i]);
+
 					vueltas[i] = monedicas;
-					resto -= monedicas * BILLETES_MONEDAS[i];
+					resto = resto % BILLETES_MONEDAS[i];
+//					resto -= ((float) monedicas) * BILLETES_MONEDAS[i];
+//					resto = Float.parseFloat(df.format(resto).replace(',', '.'));
 				}
 				if (resto == 0) {
 					break;
@@ -33,13 +44,13 @@ public class Vueltas {
 	}
 
 	public static float calcularVueltas(float importe, float entregado) throws Exception {
-		float diferencia = 0;
+		float vueltas = 0;
 		if (entregado < importe) {
 			throw new Exception("Entregado menor que importe");
 		} else {
-			diferencia = entregado - importe;
+			vueltas = entregado - importe;
 		}
-		return diferencia;
+		return vueltas;
 	}
 
 }
